972ded074c846524728e2f53a332d088944cfd71
PHawth200 <99894613+PHawth200@users.noreply.github.com>
2022-03-03 15:39:41 +0000

    Add files via upload

---
+++ Source/PluginEditor.h
@@ -0,0 +1,50 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+#include "PluginProcessor.h"
+
+//==============================================================================
+/**
+*/
+class LadderFilterAudioProcessorEditor  : public juce::AudioProcessorEditor
+{
+public:
+    LadderFilterAudioProcessorEditor (LadderFilterAudioProcessor&, juce::AudioProcessorValueTreeState&);
+    ~LadderFilterAudioProcessorEditor() override;
+
+    //==============================================================================
+    void paint (juce::Graphics&) override;
+    void resized() override;
+
+private:
+
+    // Controls for GUI Ladder Filter 
+    juce::Slider cutoffDial;
+    juce::Slider resonanceDial;
+    juce::Slider driveDial;
+    juce::ComboBox modeSel;
+    juce::ToggleButton enabled;
+
+    // Attachment between valueState and Control
+    std::unique_ptr <juce::AudioProcessorValueTreeState::SliderAttachment> cutoffValue;
+    std::unique_ptr <juce::AudioProcessorValueTreeState::SliderAttachment> resonanceValue;
+    std::unique_ptr <juce::AudioProcessorValueTreeState::SliderAttachment> driveValue;
+    std::unique_ptr <juce::AudioProcessorValueTreeState::ComboBoxAttachment> modeChoice;
+    std::unique_ptr <juce::AudioProcessorValueTreeState::ButtonAttachment> enabledToggle; 
+
+
+    // This reference is provided as a quick way for your editor to
+    // access the processor object that created it.
+    LadderFilterAudioProcessor& audioProcessor;
+    juce::AudioProcessorValueTreeState& treeState;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (LadderFilterAudioProcessorEditor)
+};
